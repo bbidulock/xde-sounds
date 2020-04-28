c3a26f335a902e41cb8823926ccc62748b5443a7
Brian Bidulock <bidulock@openss7.org>
2020-04-28 15:08:48 -0600

    add some development scripts

---
+++ src/sounds/sayit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "arguments are" $*
+
+for id in $*
+do
+	while true
+	do
+		echo "testing $id ..."
+		f="freedesktop/stereo/$id.sound"
+		if [ -s "$f" ]
+		then
+			sw=$(cat "$f"|sed -n '/^SpokenWords=/{s,SpokenWords=,,;p;q}')
+			dn=$(cat "$f"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
+			text=
+			[ -z "$text" ] && text="$sw"
+			[ -z "$text" ] && text="$dn"
+			if [ -n "$text" ]
+			then
+				echo "==> Speaking:"
+				echo "    \"$text\""
+				flite_cmu_us_slt -t "$text"
+				f="flite-F/stereo/$id.oga"
+				if [ -s "$f" ]
+				then
+					echo "==> Playing $f:"
+					echo "    \"$dn\""
+					sndfile-play "$f"
+				fi
+			else
+				echo "WARNING: missing text for $id" >&2
+			fi
+		fi
+		prefix=$(echo "$id"|sed 's,-[^-]*$,,')
+		if [ "$prefix" = "$id" ]
+		then
+			break
+		fi
+		id="$prefix"
+	done
+done
