a6569a80c0d1b13c6067f5ffc26ccaa1deccf103
Brian Bidulock <bidulock@openss7.org>
2020-04-28 15:09:20 -0600

    handle disabling sound files during synthesis
    - some events are not usefull when speech synthesized (way too
      long for too short an event)
    - this is true of many "feedback" sounds, especially window manager
      and toolkit sound
    - this change permits adding Disabled=true to sound data for an
      event and have the synthesizer skip and disable the event instead

--- src/sounds/synthesize.sh
+++ src/sounds/synthesize.sh
@@ -6,18 +6,33 @@ for f in $files ; do
 	b=$(basename "$f" .sound)
 	g="$b.wav"
 	h="$b.oga"
-	if [ ! -s "flite-M/stereo/$h" ] ; then
-		text=$(cat "$f"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
-		if [ -n "$text" ]; then
-			echo "==> Syntesizing $g:"
-			echo "    \"$text\""
-			flite_cmu_us_kal16 -t "$text" -o $g
-			echo "--> converting $g to flite-M/stereo/$h"
-			sndfile-convert -normalize -vorbis $g flite-M/stereo/$h
-			rm -fv $g
+	d="$b.disabled"
+	text=
+	[ -z "$text" ] && text=$(cat "$f"|sed -n '/^SpokenWords=/{s,SpokenWords=,,;p;q}')
+	[ -z "$text" ] && text=$(cat "$f"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
+	if [ -n "$text" ]; then
+		disabled=$(cat "$f"|sed -n '/^Disabled=/{s,Disabled=,,;p;q}')
+		if [ "$disabled" != "true" ]; then
+			if [ ! -s "flite-M/stereo/$h" ] ; then
+				echo "==> Synthesizing $g:"
+				echo "    \"$text\""
+				flite_cmu_us_kal16 -t "$text" -o $g
+				echo "--> converting $g to flite-M/stereo/$h"
+#				sndfile-convert -normalize -vorbis $g flite-M/stereo/$h
+				sndfile-convert -vorbis $g flite-M/stereo/$h
+			fi
+			rm -fv "$g" "flite-M/stereo/$d"
 		else
-			echo "WARNING: missing text for $b" >&2
+			if [ ! -f "flite-M/stereo/$d" ] ; then
+				echo "==> Disabling $d:"
+				echo "    \"$text\""
+				echo "--> creating flite-M/stereo/$d"
+				cat /dev/null >flite-M/stereo/$d
+			fi
+			rm -fv "$g" "flite-M/stereo/$h"
 		fi
+	else
+		echo "WARNING: missing text for $b" >&2
 	fi
 done
 
@@ -25,18 +40,33 @@ for f in $files ; do
 	b=$(basename "$f" .sound)
 	g="$b.wav"
 	h="$b.oga"
-	if [ ! -s "flite-F/stereo/$h" ] ; then
-		text=$(cat "$f"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
-		if [ -n "$text" ]; then
-			echo "==> Syntesizing $g:"
-			echo "    \"$text\""
-			flite_cmu_us_slt -t "$text" -o $g
-			echo "--> converting $g to flite-F/stereo/$h"
-			sndfile-convert -normalize -vorbis $g flite-F/stereo/$h
-			rm -fv $g
+	d="$b.disabled"
+	text=
+	[ -z "$text" ] && text=$(cat "$f"|sed -n '/^SpokenWords=/{s,SpokenWords=,,;p;q}')
+	[ -z "$text" ] && text=$(cat "$f"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
+	if [ -n "$text" ]; then
+		disabled=$(cat "$f"|sed -n '/^Disabled=/{s,Disabled=,,;p;q}')
+		if [ "$disabled" != "true" ]; then
+			if [ ! -s "flite-F/stereo/$h" ] ; then
+				echo "==> Synthesizing $g:"
+				echo "    \"$text\""
+				flite_cmu_us_slt -t "$text" -o $g
+				echo "--> converting $g to flite-F/stereo/$h"
+#				sndfile-convert -normalize -vorbis $g flite-F/stereo/$h
+				sndfile-convert -vorbis $g flite-F/stereo/$h
+			fi
+			rm -fv "$g" "flite-F/stereo/$d"
 		else
-			echo "WARNING: missing text for $b" >&2
+			if [ ! -f "flite-F/stereo/$d" ] ; then
+				echo "==> Disabling $d:"
+				echo "    \"$text\""
+				echo "--> creating flite-F/stereo/$d"
+				cat /dev/null >flite-F/stereo/$d
+			fi
+			rm -fv "$g" "flite-F/stereo/$h"
 		fi
+	else
+		echo "WARNING: missing text for $b" >&2
 	fi
 done
 
