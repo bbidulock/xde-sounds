8feab3211fe70a886554571abe3162bf77b5116c
Brian Bidulock <bidulock@openss7.org>
2020-04-30 02:15:27 -0600

    set up development scripts to use sox, voices and effects

--- src/sounds/sayitm.sh
+++ src/sounds/sayitm.sh
@@ -2,39 +2,67 @@
 
 echo "arguments are" $*
 
+sexes="M"
+
+declare -A subdir
+declare -A defvox
+declare -A voxs
+
+subdir=([F]=flite-F [M]=flite-M)
+defvox=([F]=cmu_us_slt [M]=cmu_us_rms)
+voxs=([F]="axb clb eey ljm lnh slp slt" [M]="aup fem gka ahw rxr bdl jmk awb ksp aew rms")
+
+echo "subdir[F] = ${subdir[F]}"
+echo "subdir[M] = ${subdir[M]}"
+echo "defvox[F] = ${defvox[F]}"
+echo "defvox[M] = ${defvox[M]}"
+echo "voxes[F] = ${voxes[F]}"
+echo "voxes[M] = ${voxes[M]}"
+
 for id in $*
 do
 #	while true
 #	do
 #		echo "testing $id ..."
 		echo "==>" $id
-		f="freedesktop/stereo/$id.sound"
-		if [ -s "$f" ]
+		file="freedesktop/stereo/$id.sound"
+		if [ -s "$file" ]
 		then
-			sw=$(cat "$f"|sed -n '/^SpokenWords=/{s,SpokenWords=,,;p;q}')
-			dn=$(cat "$f"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
+			sw=$(cat "$file"|sed -n '/^SpokenWords=/{s,SpokenWords=,,;p;q}')
+			dn=$(cat "$file"|sed -n '/^DisplayName=/{s,DisplayName=,,;p;q}')
 			text=
 			[ -z "$text" ] && text="$sw"
 			[ -z "$text" ] && text="$dn"
 			if [ -n "$text" ]
 			then
-				 #for v in aup fem gka ahw rxr bdl jmk awb ksp aew rms; do
-				for v in rms; do
-					echo " -> Speaking: $v"
+				for sex in $sexes ; do
+					voice=$(cat "$file"|sed -n "/^Speaker[[]$sex[]]=/{s,Speaker[[]$sex[]]=,,;p;q}")
+					echo " -> Speaking (w/o effects): $sex ${voice:-${defvox[$sex]}}"
 					echo "    \"$text\""
-					flite -voice /usr/lib/flite/cmu_us_${v}.flitevox -t "$text"
-					sleep 1
+					flite -voice /usr/lib/flite/${voice:-${defvox[$sex]}}.flitevox -t "$text" -o /dev/stdout | \
+						play -t wav - gain -n -3
+					effect=$(cat "$file"|sed -n '/^SoxEffects=/{s,SoxEffects=,,;p;q}')
+					if test -n "$effect" ; then
+						echo " -> Speaking (w/  effects): $sex ${voice:-${defvox[$sex]}}"
+						echo "    \"$text\""
+						flite -voice /usr/lib/flite/${voice:-${defvox[$sex]}}.flitevox -t "$text" -o /dev/stdout | \
+							play -t wav - ${effect} gain -n -3
+					else
+						echo " -> No effects for $file (skipping)"
+					fi
+					play="${subdir[$sex]}/stereo/$id.ogg"
+					[ -s "$play" ] || play="${subdir[$sex]}/stereo/$id.oga"
+					[ -s "$play" ] || play="${subdir[$sex]}/stereo/$id.wav"
+					if [ -s "$play" ]
+					then
+						echo " -> Playing $sex $play:"
+						echo "    \"$dn\""
+						echo -n "    "
+						play "$play"
+					else
+						echo " -> No file for $play (skipping)"
+					fi
 				done
-				f="flite-M/stereo/$id.ogg"
-				[ -s "$f" ] || f="flite-M/stereo/$id.oga"
-				[ -s "$f" ] || f="flite-M/stereo/$id.wav"
-				if [ -s "$f" ]
-				then
-					echo " -> Playing $f:"
-					echo "    \"$dn\""
-					echo -n "    "
-					sndfile-play "$f"
-				fi
 			else
 				echo "WARNING: missing text for $id" >&2
 			fi
